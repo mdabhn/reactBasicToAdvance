b909253e977e2d6dc449ecaad49de13e0708f32a
mdabhn <mdabhn@gmail.com>
2020-10-20 20:25:45 +0600

    HEADER COMPONETS ADDED 😎😎😎

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import HomePage from './pages/HomePage';
 import Shop from './pages/shop/Shop';
+import Header from './components/header/Header';
 import { Route } from 'react-router-dom';
 
 function App() {
   return (
     <div>
+      <Header />
       <Route exact path='/' component={HomePage} />
       <Route exact path='/shop' component={Shop} />
     </div>
